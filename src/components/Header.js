903903a13bd318354aa62f5f9e7cc925913ea15b
shooters00 <shooters0000@gmail.com>
2021-10-17 10:43:04 -0400

    Centering pic better

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ import headshot from "../assets/images/headshot.jpg";
 const headStyle = {
 //  marginTop: "50px",
 //  marginRight: "30px",
-//  textAlign: "center",
+  textAlign: "center",
   
 };
 
 export default function Header() {
   return (
     <div className="back-pic">
-      <img className="ed col" src={headshot} alt="Page author"></img>
+      <img style={headStyle} className="ed col" src={headshot} alt="Page author"></img>
       <div className="banner col">
       <h1 className="name col">Edward Fatula</h1>
       <h2 className="welcome col">Welcome to my portfolio!</h2>
